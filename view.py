862df681ff89722dccaaa829e71dd2c3560e2ead
Lucas Duarte <luckasdu686@gmail.com>
2022-12-22 22:06:00 -0300

    Criando Pagina Inicial

--- view.py
+++ view.py
@@ -3,7 +3,8 @@ from tkinter import *
 
 # -------------- Cores -------------------
 
-cinza = "#ccc"
+co1 = "#ccc"
+co2 = "#069"
 
 
 # -------------- Atributos Janela --------------
@@ -11,11 +12,15 @@ Home = Tk()
 Home.title("Estoque Materiais")
 icone = PhotoImage(file="images/parcela.png")
 Home.iconphoto(False, icone)
-Home.configure(bg= cinza)
+Home.configure(bg= co1)
 Home.state('zoomed')    # Preenche toda a tela
 
+frame_topo = Frame(Home, width=1500, height=200,bg= co2)
+frame_topo.grid(column=0, row=1)
 
-
+logo = PhotoImage(file="images/logo.png", width=200, height=200)
+logo_label = Label(frame_topo, image= logo, bg= co1)
+logo_label.place(x= 0, y=0)
 
 
 
